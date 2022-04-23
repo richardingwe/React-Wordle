07d9d35c7408f7671e4cae9498c0ef795449a4e7
Richard Ingwe <richardingwe5@gmail.com>
2022-04-23 16:17:58 +0100

    add json data and get solution word

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import './App.css';
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        // random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {
+        solution &&
+        <div>Solution is: {solution}</div>
+      }
     </div>
   );
 }
 
-export default App
+export default App;
 
-/* 
+/*
 
 data we need to track:
   -- solution
